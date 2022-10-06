62856d0f0b4b1dc0346dda4b9367a8bedabb4e90
sudalS2 <98267987+sudalS2@users.noreply.github.com>
2022-10-06 13:44:49 +0900

    Update Code.py

--- Code.py
+++ Code.py
@@ -16,7 +16,7 @@ A  B  C
 3  32 33
 
 # 예제 1-2)
-## DLM=',' 
+## DLM 옵션
 DATA scores;
   INFILE CARDS DLM=','
   INPT test1 test2 test3;
@@ -27,11 +27,12 @@ CARDS;
 ;
 RUN;
 
-# 결과
+# 결과: 인용부호 안에 있는 문자는 구분자로 인식하지 않고, 데이터 값으로 받아들이지 않음. 
  test1 test2 test3
   91    87    95
   97    92    1
 
+## DSD 옵션 1
 DATA scores;
   INFILE CARDS DSD;
   INPT test1 test2 test3;
@@ -42,4 +43,28 @@ CARDS;
 ;
 RUN;
 
+# 결과: 연속된 구분자들 사이를 결측값으로 처리
+ test1 test2 test3
+  91    87    95
+  97    .     92
+  1     1     1
+  
+## DSD 옵션 2
+DATA topics;
+  INFILE CARDS DSD;
+  INPUT speaker : $1. title : $40. location $ $10.; # 포맷 수정자: :(콜론)은 고정포맷으로 데이터를 읽는 경우에 구분자까지만 변수 값을 읽도록 함.
+CARDS;
+Whitfield, "Looking at  Water. Looking at Life", Blue Room
+Fuentes, "Life After the Ravolution", Red Room
+TOWNSEND, "Peace in Our Times", Green Room
+;
+RUN;
+
+# 결과
+speaker                title                     location
+Whitfield   Looking at  Water. Looking at Life   Blue Room
+Fuentes     Life After the Ravolution            Red Room
+TOWNSEND    Peace in Our Times                  Green Room
+  
+ 
   
